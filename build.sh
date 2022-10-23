1111f7b62199a3181315ea0cf7750311df3e478a
Qwreey <qwreey75@gmail.com>
2022-10-23 18:46:16 +0900

    fixed build script

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+schemas/build.sh
 rm -rf dist 2> /dev/null
 mkdir dist 2> /dev/null
-cp extension.js dist/
-cp prefs.js dist/
-cp metadata.json dist/
-mkdir dist/schemas
-cp schemas/qwreey.hide_quick_buttons.gschema.xml dist/schemas/
-cp schemas/gschemas.compiled dist/schemas/
+mkdir dist/build 2> /dev/null
+cp extension.js dist/build/
+cp prefs.js dist/build/
+cp metadata.json dist/build/
+mkdir dist/build/schemas
+cp schemas/qwreey.hide_quick_buttons.gschema.xml dist/build/schemas/
+cp schemas/gschemas.compiled dist/build/schemas/
+zip -r dist/output.zip dist/build
